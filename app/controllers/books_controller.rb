aadd285981ed151761d8c7f378e903b3e0545a18
rinahanyu <gravity0112@gmail.com>
2020-09-28 06:50:04 +0000

    [Add]Rubocop

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @books = Book.all
     @book = Book.new(book_params)
@@ -14,15 +14,15 @@ class BooksController < ApplicationController
       render("books/index")
     end
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -32,15 +32,16 @@ class BooksController < ApplicationController
       render("books/edit")
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
-  
+
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
